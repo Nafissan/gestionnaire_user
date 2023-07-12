8fe3d21f23a7f2989b0ea0e6e87fc403beec8e5c
Nafissan <101016522+Nafissan@users.noreply.github.com>
2023-07-12 20:15:39 +0000

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import mysql.connector
+import hashlib
 
 app = Flask(__name__)
 
@@ -16,7 +17,8 @@ db = mysql.connector.connect(
 def authentification():
     data = request.get_json()
     username = data['username']
-    password = data['password']
+    password = hashlib.md5(data['password'].encode()).hexdigest()
+
 
     # Vérification des informations d'authentification dans la base de données
     cursor = db.cursor()
@@ -34,7 +36,7 @@ def authentification():
 def ajouter_utilisateur():
     data = request.get_json()
     username = data['username']
-    password = data['password']
+    password = hashlib.md5(data['password'].encode()).hexdigest()
     # Ajout de l'utilisateur dans la base de données
     cursor = db.cursor()
     cursor.execute("INSERT INTO utilisateur (login, password) VALUES (%s, %s)", (username, password))
@@ -47,27 +49,42 @@ def ajouter_utilisateur():
 @app.route('/utilisateurs/<int:user_id>', methods=['PUT'])
 def modifier_utilisateur(user_id):
     data = request.get_json()
-    new_password = data['password']
+    new_password = hashlib.md5(data['password'].encode()).hexdigest()
     new_username = data['username']
 
     # Modification du mot de passe de l'utilisateur dans la base de données
     cursor = db.cursor()
-    cursor.execute("UPDATE utilisateur SET login= %s ,password = %s WHERE id = %s", (new_username,new_password, user_id))
-    db.commit()
+    cursor.execute("SELECT * FROM utilisateur WHERE id = %s", (user_id,))
+    user = cursor.fetchone()
     cursor.close()
 
-    return jsonify({'message': 'Utilisateur modifié avec succès'})
+    if not user:
+        return jsonify({'message': 'Utilisateur non trouvé'})
+    else:
+        cursor = db.cursor()
+        cursor.execute("UPDATE utilisateur SET login= %s ,password = %s WHERE id = %s", (new_username,new_password, user_id))
+        db.commit()
+        cursor.close()
+        return jsonify({'message': 'Utilisateur modifié avec succès'})
 
+      
 # Route pour la suppression d'un utilisateur
 @app.route('/utilisateurs/<int:user_id>', methods=['DELETE'])
 def supprimer_utilisateur(user_id):
     # Suppression de l'utilisateur de la base de données
     cursor = db.cursor()
-    cursor.execute("DELETE FROM utilisateur WHERE id = %s", (user_id,))
-    db.commit()
+    cursor.execute("SELECT * FROM utilisateur WHERE id = %s", (user_id,))
+    user = cursor.fetchone()
     cursor.close()
 
-    return jsonify({'message': 'Utilisateur supprimé avec succès'})
+    if not user:
+        return jsonify({'message': 'Utilisateur non trouvé'})
+    else :
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM utilisateur WHERE id = %s", (user_id,))
+        db.commit()
+        cursor.close()
+        return jsonify({'message': 'Utilisateur supprimé avec succès'})
 
 # Route pour lister tous les utilisateurs
 @app.route('/utilisateurs', methods=['GET'])
